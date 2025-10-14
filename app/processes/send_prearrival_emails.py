fa740fa82cbc48fea4df4ae1cee259bfc8cacecf
Francisco Viñas <fvinas@fi.uba.ar>
2025-10-14 09:09:28 -0300

    fix: remove specific email filter for pre-arrival applications

--- app/processes/send_prearrival_emails.py
+++ app/processes/send_prearrival_emails.py
@@ -126,7 +126,6 @@ def get_applications_for_prearrival(db: Session):
         .filter(
             PopUpCity.slug == POPUP_CITY_SLUG,
             Application.email.notin_(excluded_emails),
-            Application.email == 'francisco@muvinai.com',
         )
         .distinct()
         .all()
